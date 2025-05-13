204f0347188c0087f656f19f4a1ed18793b6ca7c
Kayraakbas <kayraakbas7gmail.com>
2025-05-13 10:58:20 +0300

    enes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Root build.gradle.kts
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+    id("com.android.application") version "8.8.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+}
+
+buildscript {
+    dependencies {
+        classpath (libs.google.services) // bu satırı ekle
+    }
 }
